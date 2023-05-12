229baac434e8e933507266a5565c9de6131dfd06
Yagth <yabuman46@gmail.com>
2023-05-12 21:10:47 +0300

    Started working on user get api

---
+++ Backend/api/user/get_users.php
@@ -0,0 +1,43 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/User.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$user = new User($db);
+$result = $user->get_user();
+
+$num  = $result->num_rows;
+
+if($num > 0){
+
+   $user_arr = array();
+   $user_arr['data'] = array();
+
+   while($row = $result->fetch_assoc()){
+       extract($row);
+
+       $user_item = array(
+           'id' => $id,
+           'title' => $title,
+           'body' => html_entity_decode($body),
+           'author' => $author,
+           'category_id' => $category_id,
+           'category_name' => $category_name
+       );
+       
+       array_push($user_arr['data'], $user_item);
+   }
+
+   echo json_encode($user_arr);
+
+}else{
+   echo json_encode(
+       array('message' => 'No such users')
+   );
+}
---
+++ Backend/models/User.php
@@ -0,0 +1,94 @@
+<?php
+class User{
+    //DB Stuff
+    private $conn;
+    private $table = 'user';
+
+    //Posts properties
+
+    public $id;
+    public $name;
+    public $password;
+    public $email;
+
+    //Constructor 
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    //Get posts
+
+    public function get_user (){
+        //Create query
+        $query = "SELECT * FROM $this->table";
+
+        $stmt = $this->conn->stmt_init();
+        if(!$stmt->prepare($query)){
+            die("SQL Error: " . $this->conn->error);
+        }
+                
+        try{
+            $stmt->execute();
+        } catch(mysqli_sql_exception $e){
+            die(($this->conn->error . " " . $this->conn->errno));
+        }
+        //Prepared statements
+        return $stmt->get_result();
+    }
+
+    public function get_single_user (){
+        //Create query
+        $query = "SELECT * FROM $this->table WHERE email = ?
+            LIMIT 0,1";
+
+        $stmt = $this->conn->stmt_init();
+
+        if(!$stmt->prepare($query)){
+            die("SQL Error: " . $this->conn->error);
+        }
+        
+        $stmt->bind_param("s", $this->email);
+        
+        try{
+            $stmt->execute();
+        } catch(mysqli_sql_exception $e){
+            die(($this->conn->error . " " . $this->conn->errno));
+        }
+        if($stmt->get_result()->num_rows > 0){
+            $row = $stmt->get_result()->fetch_assoc();
+
+            //Set properties
+            $this->email = $row['email'];
+            $this->name = $row['name'];
+            $this->password = $row['password'];
+            $this->id = $row['id'];
+        }
+    }
+
+    public function create_user(){
+        $query = "INSERT INTO $this->table 
+                SET name = ?, password = ?, email = ?";
+
+        $stmt = $this->conn->stmt_init();
+
+        if(!$stmt->prepare($query)){
+            die("SQL Error: " . $this->conn->error);
+        }
+        
+        $this->name = $this->conn->real_escape_string($this->name);
+        $this->password = $this->conn->real_escape_string($this->password);
+        $this->email = $this->conn->real_escape_string($this->email);
+
+        $stmt->bind_param("sss", $this->name, $this->password, $this->email);
+        
+        try{
+            $stmt->execute();
+            return true;
+        } catch(mysqli_sql_exception $e){
+            printf ("Error: %s.\n", $e);
+            return false;
+        }
+    }
+
+}
