c0e320673b5a13e28df1d72e098632f283100241
Yagth <yabuman46@gmail.com>
2023-06-10 19:35:54 +0300

    Suppressed warning of field not found

---
+++ Backend/api/house/update_house.php
@@ -0,0 +1,51 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/House.php';
+include_once '../../models/User.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$house = new House($db);
+
+if(isset($_POST['submit']) && isset($_FILES['new_image']) && isset($_POST['house_id'])){
+    $house_id = $_POST['house_id'];
+    $house->get_single_house($house_id);
+    
+    $house->price      = isset($_POST['price'])      ? $_POST['price'] : $house->price;
+    $house->house_desc = isset($_POST['house_desc']) ? $_POST['house_desc'] : $house->house_desc;
+    $house->no_rooms   = isset($_POST['no_rooms'])   ? $_POST['no_rooms'] : $house->no_rooms;
+    $house->owner_id   = isset($_POST['owner_id'])   ? $_POST['owner_id'] : $house->owner_id;
+    $house->status     = isset($_POST['status'])     ? $_POST['status'] : $house->status;
+    
+    $updated = $house->update_house($house_id);
+    
+    if(isset($_POST['pic_id'])){
+        $pic_id = $_POST['pic_id'];
+        $pic_desc = $_POST['pic_desc'];
+        $pic_url = $house->image_upload($_FILES['new_image']);
+
+        $updated |= $house->update_house_pics($pic_id, $pic_desc, $pic_url);
+    }
+}
+if($updated){
+
+   echo json_encode(
+    array(
+     'sucess' => True,
+     'message' => 'Update Success'
+    )
+);
+
+}else{
+   echo json_encode(
+       array(
+        'sucess' => False,
+        'message' => 'Update Failed'
+        )
+   );
+}
