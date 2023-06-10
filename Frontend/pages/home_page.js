f89d25480ba31b1508b35b9404185eb9c07dda88
Yagth <yabuman46@gmail.com>
2023-06-10 20:43:29 +0300

    Working on carousal bug fix

--- Frontend/pages/home_page.js
+++ Frontend/pages/home_page.js
@@ -1,8 +1,9 @@
-img1 = document.getElementById("img1");
-img2 = document.getElementById("img2");
-img3 = document.getElementById("img3");
-forwardButton = document.getElementsById("forward-button");
+forwardButton = document.getElementById("forward-button");
 forwardButton.addEventListener("click", () => {
+  img1 = document.getElementById("img1");
+  img2 = document.getElementById("img2");
+  img3 = document.getElementById("img3");
+  console.log(img1.style.opacity);
   if (img1.style.opacity == "1") {
     img1.style.opacity = "0";
     img2.style.opacity = "1";
