469b90677460f6358f7258b6b3b840c7ab3183e4
Yagth <yabuman46@gmail.com>
2023-06-11 21:20:18 +0300

    Added some log message upon signup

--- Frontend/pages/signup.js
+++ Frontend/pages/signup.js
@@ -2,6 +2,7 @@ let submit = document.getElementById("submit");
 submit.addEventListener("click", async () => {
   try {
     let form = document.getElementById("signup");
+    let errorHeader = document.getElementById("error_header");
     const formData = new FormData(form);
     const res = await fetch(
       "http://localhost:8080/PHP/AddisHouseRental/Backend/api/auth/signup.php",
@@ -11,7 +12,13 @@ submit.addEventListener("click", async () => {
       }
     );
     const data = await res.json();
-    console.log(data);
+    if (data.success) {
+      errorHeader.textContent = "Signup successful";
+      console.log(data.data);
+    } else {
+      errorHeader.textContent = data.error;
+      console.log(data.error);
+    }
   } catch (error) {
     console.log("error" + error.message);
   }
