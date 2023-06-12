0df99662681638fedd5bc1abd65c08092c47d39e
Yagth <yabuman46@gmail.com>
2023-06-13 01:46:44 +0300

    Refactored the create house response type

--- Backend/api/house/create_house.php
+++ Backend/api/house/create_house.php
@@ -94,7 +94,10 @@ if(isset($_POST['submit']) && isset($_FILES['main_pic'])){
                              "owner_id" => (int)$house->owner_id, 
                              "price" => (double)$house->price, 
                              "desc" =>$house->house_desc, 
-                             "rooms" => (int)$house->no_rooms, 
+                             "rooms" => (int)isset($house->no_rooms)?$house->no_rooms: 0,
+                             "bed_rooms" => (int)isset($house->bed_rooms)?$house->bed_rooms:0,
+                             "bath_rooms" => (int)isset($house->bath_rooms)?$house->bath_rooms:0,
+                             "house_tag" => $house->house_tag,
                              "status" => $house->status,
                              "pictures" => $house->house_pics
                             );
