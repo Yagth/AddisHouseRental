4f78e58b81fe041f7d38b41cb195423bd958fe9d
Yagth <yabuman46@gmail.com>
2023-06-19 19:38:01 +0300

    Added componenet to the property lists page

--- Frontend/js/property_page.js
+++ Frontend/js/property_page.js
@@ -7,44 +7,6 @@ let apartmentB = $("#apartment");
 let villaB = $("#villa");
 let houseB = $("#house");
 
-const loadHouses = async () => {
-  let container = document.querySelector(".card_div");
-  let card = document.querySelector(".card_div .card");
-  let data = await getData(
-    "http://localhost:8080/PHP/AddisHouseRental/Backend/api/house/get_house.php"
-  );
-  if (data.success) {
-    let houses = shuffleArray(data.data);
-    if (houses.length > 6) {
-      houses = houses.slice(0, 12);
-    }
-    houses.forEach((house) => {
-      let newCard = card.cloneNode(true);
-      let isHouse = ["Apartment", "Villa", "Home"].includes(house.house_tag);
-
-      card.attributes.id = house.id;
-      card.querySelector(".card_text1").textContent = "$" + house.price;
-      card.querySelector(".card_text2").textContent = house.house_description;
-      card.querySelector(".card_text3").textContent = house.location;
-      let foots = card.querySelectorAll(".card-foot-c");
-      foots[0].textContent =
-        (isHouse ? house.bed_rooms + house.bath_rooms : house.rooms) + " Rooms";
-      foots[1].textContent = house.bed_rooms + " Beds";
-      foots[2].textContent = house.bath_rooms + " Baths";
-      card.querySelector("img").src =
-        "http://127.0.0.1:5500/Backend/uploads/img/houses/" +
-        house.pics[0][0]?.photo_url;
-      card.classList.add("visible-card");
-      container.appendChild(card);
-      card.classList.remove("hidden");
-      card = newCard;
-    });
-    saveCookie("houses", { houseArray: houses });
-  } else {
-    console.log(data.message);
-  }
-};
-
 const filterHousesByTag = (tag) => {
   let housesDiv = document.querySelector(".card_div");
   let houseElements = housesDiv.children;
@@ -83,13 +45,6 @@ houseB.on("click", () => {
   houseB.addClass("button-active").removeClass("button-featured");
 
   filterHousesByTag("House");
-});
-
-let startButton = document.getElementById("start");
-startButton.addEventListener("click", () => {
-  console.log("inside action listener");
-  window.location.href =
-    "http://127.0.0.1:5500/Frontend/pages/signup_page.html";
 });
 
 window.addEventListener("scroll", () => {
@@ -138,5 +93,3 @@ if (user) {
       "http://127.0.0.1:5500/Frontend/pages/login_page.html";
   });
 }
-
-loadHouses();
